7a5e8129831d1da8454c5ae565e9fec9f0c73da0
kl68884 <rick.li@citi.com>
2014-04-11 11:03:24 +0800

    add message history

--- node/socketio/server.js
+++ node/socketio/server.js
@@ -1,28 +1,53 @@
-var path = require('path');
 var express = require('express');
+var mongoose = require('mongoose');
 
+var env = process.env.NODE_ENV;
+console.log('ENV is ', env);
+//================ Mongoose================
+var localMongoConn = 'mongodb://cvmobilevm.apac.nsroot.net:27017/chat-poc';
+var remoteMongoConn = 'mongodb://rick-li:citi1234@ds043047.mongolab.com:43047/chat-poc';
+var mongoConn = env === 'local' ? localMongoConn : remoteMongoConn;
+mongoose.connect(mongoConn);
+var Message = mongoose.model('Message', {
+  text: String,
+  address: {
+    address: String,
+    port: Number
+  }
+});
+
+//================Express===================
 var app = require('express')();
-console.log('ENV is ', process.env.NODE_ENV);
-var port = (process.env.NODE_ENV == 'local') ? 3000 : 80;
+var port = env === 'local' ? 3000 : 80;
 console.log('Use port ', port);
 app.set('port', port);
-// console.log(__dirname + '');
 app.use(express.static(__dirname));
 app.use(app.router);
 app.use(express.json());
-
-
 var server = require('http').createServer(app).listen(port);
+
+app.get('/historyMessages', function(req, res, next) {
+  Message.find().limit(20).exec(function(err, msgs) {
+    if (err) {
+      console.log('Error ', err);
+      next();
+    }
+    console.log('messages ' + msgs.length);
+    res.send(msgs);
+  });
+});
+
+//================SocketIO===================
 var io = require('socket.io').listen(server);
-
-
 io.sockets.on('connection', function(socket) {
   var address = socket.handshake.address;
   console.log('New connection established from ', address);
 
-  socket.on('clientMsg', function(data) {
-    console.log('received:', address, data);
-    data.address = address;
-    io.sockets.emit('serverMsg', data);
+  socket.on('clientMsg', function(message) {
+    console.log('received:', address, message);
+    message.address = address;
+    io.sockets.emit('serverMsg', message);
+    var mMsg = new Message(message);
+    mMsg.save();
   });
 });
